9af484bc6f57026ed0446890e86cc4ed8d46ba39
NNA Design <nabilahatiqah12@gmail.com>
2025-05-20 17:45:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-// Countdown to wedding date
-const countdownDate = new Date("June 1, 2025 10:00:00").getTime();
 
-const countdownElement = document.getElementById("countdown");
-
-function updateCountdown() {
-  const now = new Date().getTime();
-  const timeLeft = countdownDate - now;
-
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-  countdownElement.innerHTML = `Wedding in ${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-  if (timeLeft < 0) {
-    clearInterval(countdownInterval);
-    countdownElement.innerHTML = "Wedding has started!";
-  }
-}
-
-const countdownInterval = setInterval(updateCountdown, 1000);
+document.getElementById("start-btn").addEventListener("click", function() {
+  var audio = document.getElementById("bg-music");
+  audio.play();
+});
