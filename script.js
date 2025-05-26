ab2cb683e714bef4b5ed86b9ccd349286297a945
NNA Design <nabilahatiqah12@gmail.com>
2025-05-26 22:47:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-
-document.getElementById("start-btn").addEventListener("click", function() {
-  var audio = document.getElementById("bg-music");
-  audio.play();
-});
+// script.js
+fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTPi4cVVJVAYtYrDQRfhBMX0qCMllBMgjYqesb64WKf-5M4BvxIrabnse_Fq_Iu6EHsrnI8Rv1AEv7T/pub?output=csv')
+  .then(response => response.text())
+  .then(data => {
+    const rows = data.split("\n").map(row => row.split(","));
+    rows.forEach(row => {
+      const barang = row[2].trim();
+      if (barang === "Bakul Baju") {
+        const btn = document.getElementById("btn-bakul");
+        btn.innerText = "Telah Ditempah";
+        btn.disabled = true;
+      }
+    });
+  });
